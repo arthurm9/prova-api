d6f768c8ef85920bf1a5017866e2c49374ebe8a3
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-05-03 22:57:29 -0300

    add dependencies; and instance mockmvc

---
+++ src/test/java/br/com/etec/arthur/provaapi/resource/ClienteResourceTest.java
@@ -0,0 +1,22 @@
+package br.com.etec.arthur.provaapi.resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ClienteResourceTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void listar {
+
+    }
+}
